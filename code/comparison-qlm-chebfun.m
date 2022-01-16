936c2f4de386ef01048b567bffa49501a8e8b022
Jesus-Octavio <91897172+Jesus-Octavio@users.noreply.github.com>
2022-01-16 10:03:44 +0100

    Add files via upload

---
+++ code/comparison-qlm-chebfun.m
@@ -0,0 +1,38 @@
+% comparison QLM-Chebfun
+% Boyd, J. P. (2011). Chebyshev Spectral Methods and the Lane-Emden problem.
+% Numerical Mathematics: Theory, Methods and Applications, 4(2), 142-157.
+
+format longg                       % Variable precision
+
+N = 11;                            % Number of collocation pts
+n = 3;                             % Polytropic index
+iters = 10;                        % Number Newton iterations
+ 
+[xi, Y, XCheb, a] = qlm(N,n,iters);
+a;
+R = xi.*XCheb;                     % x-data
+plot(R, Y, 'b-*')                  % Plot function
+hold on
+plot(R(1), Y(1), 'r-o')            % Mark last point. eigenvalue & 1st root
+hold on
+
+N = chebop(@(x,u) x.*diff(u,2)+2.*diff(u)+x.*u.^n,[0,1);%L-E operator
+N.lbc = @(u) [u-1; diff(u)];                 % Left boundary conditions
+u = solvebvp(N,0);                           % Solve BVP
+plot(u,'Linewidth',1.5), hold on; % Plot 
+
+text(R(1)-0.2,Y(1)+0.1,['\xi=',num2str(R(1)),'...'])
+ 
+disp('First root & eigenvalue xi =  ');
+disp(xi);
+
+% fprintf('EL primer cero de la función es:  %.2f.\n', xi); 
+% But not format longg
+
+met = 'QSLM';
+s=sprintf('Solve Lane-Emden with met=%s, Nodes N=%g, Polytropic index n=%g, Iterations=%g',met,N,n,iters);
+title({' ','QSLM. Boyd (SLM)',s,});
+
+
+
+
